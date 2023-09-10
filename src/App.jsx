dbd6e9c3ebbd19b096f2f9e75589e3d2628bd316
alejomaar <demonmaar@gmail.com>
2023-09-09 23:39:06 -0500

    width responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen h-screen">
-      <div className="mx-auto flex justify-center items-center flex-col">
+    <div className="min-h-screen h-screen ">
+      <div className="max-w-4xl mx-auto flex justify-center items-center flex-col">
+        <h2 className="font-bold text-4xl">Who i am</h2>
+        <p className="text-lg text-center">
+          I'm Alejandro, I combine the world of data and software, creating
+          products and strategies through problem solving. I am focused on
+          creating solutions based on scalability, interactive systems and
+          multidisciplinary fields.
+        </p>
+      </div>
+      <div className="max-w-4xl mx-auto flex justify-center items-center flex-col pt-3">
         <h2 className="font-bold text-4xl">Who i am</h2>
         <p className="text-lg text-center">
           I'm Alejandro, I combine the world of data and software, creating
