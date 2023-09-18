2efde7649b9a8a3b6ab0ce4cade6f74c375c9dd4
alejomaar <demonmaar@gmail.com>
2023-09-18 18:06:02 -0500

    fix(import): fix import jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage/LandingPage";
-//import LandingPage from "@pages/LandingPage";
 
 function App() {
   return <LandingPage></LandingPage>;
