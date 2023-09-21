01badbbd2b131004b91edc4933a2eda639b98f79
alejomaar <demonmaar@gmail.com>
2023-09-20 23:20:44 -0500

    feat(style|optimization): Change local images by aws s3 images

--- src/pages/LandingPage/components/Projects/assets/data.jsx
+++ src/pages/LandingPage/components/Projects/assets/data.jsx
@@ -1,20 +1,16 @@
-import background_google_art_and_culture from "@assets/google_art_and_culture.jpg";
-import background_virtual_museum from "@assets/virtual_museum.png";
-import background_mlflow from "@assets/mlflow.png";
-
 const data = [
   {
-    img: background_google_art_and_culture,
+    img: "https://aws-resources-ia.s3.us-west-1.amazonaws.com/google_art_and_culture.jpg",
     description: "landing_page.project_1_description",
     title: "landing_page.project_1_title",
   },
   {
-    img: background_virtual_museum,
+    img: "https://aws-resources-ia.s3.us-west-1.amazonaws.com/virtual_museum.png",
     description: "landing_page.project_2_description",
     title: "landing_page.project_2_title",
   },
   {
-    img: background_mlflow,
+    img: "https://aws-resources-ia.s3.us-west-1.amazonaws.com/mlflow.png",
     description: "landing_page.project_3_description",
     title: "landing_page.project_3_title",
   },
