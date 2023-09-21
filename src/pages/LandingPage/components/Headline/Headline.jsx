99643123bc383ca4b425b1cf7b4138ce57cf2af8
alejomaar <demonmaar@gmail.com>
2023-09-20 23:21:10 -0500

    feat(style): add dropdown and call to action

--- src/pages/LandingPage/components/Headline/Headline.jsx
+++ src/pages/LandingPage/components/Headline/Headline.jsx
@@ -2,34 +2,42 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import profile_photo_img from "@assets/profile_photo.png";
+import arrow from "@assets/arrow.svg";
 import { useTranslation } from "react-i18next";
 
 export default function Headline() {
   const { t } = useTranslation();
   return (
     <div className="max-w-7xl mx-auto flex justify-center items-center flex-col  h-screen">
-      <h1 className="font-bold text-2xl sm:text-3xl  text-center sans_serif">
+      <h1 className="font-bold text-xl md:text-2xl lg:text-3xl  text-center sans_serif px-4">
         {t("landing_page.greeting")}
       </h1>
-      <h1 className="font-bold px-5 text-5xl sm:text-7xl  text-gray-900 text-center ">
+      <h1 className="font-bold px-5 text-3xl md:text-5xl lg:text-7xl  text-gray-900 text-center px-4">
         {t("landing_page.occupation")}
       </h1>
 
-      <div className="w-full relative flex justify-center items-center">
-        <div className="w-full overflow-hidden absolute top-1/2 border-t border-b border-black">
-          <div className="marquee-container  my-7">
+      <div className="w-full relative flex justify-center items-center mt-14">
+        <div className="w-full overflow-hidden absolute origin-center border-t border-b border-black py-7">
+          <div className="marquee-container">
             <ul className="marquee flex">
-              <li className="mx-3 text-6xl">{t("landing_page.skills_1")}</li>
-              <li className="mx-3 text-6xl">{t("landing_page.skills_2")}</li>
-              <li className="mx-3 text-6xl">{t("landing_page.skills_3")}</li>
-              <li className="mx-3 text-6xl">{t("landing_page.skills_4")}</li>
+              <li className="mx-3 text-4xl md:text-6xl">
+                {t("landing_page.skills_1")}
+              </li>
+              <li className="mx-3 text-4xl md:text-6xl">
+                {t("landing_page.skills_2")}
+              </li>
+              <li className="mx-3 text-4xl md:text-6xl">
+                {t("landing_page.skills_3")}
+              </li>
+              <li className="mx-3 text-4xl md:text-6xl">
+                {t("landing_page.skills_4")}
+              </li>
             </ul>
           </div>
         </div>
         <img
-          className="w-64 h-64 mt-14 z-10 rounded-full"
-          src={profile_photo_img}
+          className="w-44 h-44 md:w-64 md:h-64  z-10 rounded-full"
+          src="https://aws-resources-ia.s3.us-west-1.amazonaws.com/profile_photo.png"
         />
       </div>
 
@@ -48,6 +56,10 @@ export default function Headline() {
         >
           <FontAwesomeIcon icon={faLinkedin} size="3x" color="white" />
         </a>
+      </div>
+      <div class="scroll-down absolute flex flex-col items-center right-5 md:right-12 lg:right-24 bottom-5">
+        <img class="w-4 animate-bounce" src={arrow} />
+        <p>Scroll Down</p>
       </div>
     </div>
   );
--- src/pages/LandingPage/components/Navbar/Navbar.jsx
+++ src/pages/LandingPage/components/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-scroll";
 
 export default function Navbar() {
   const [menuActive, setmenuActive] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("English");
+
   return (
     <nav className="w-full fixed top-0 left-0 right-0 flex h-20 bg-white z-20">
       <div className="w-6/12 md:w-3/12 flex ml-12 items-center ">
@@ -47,9 +50,41 @@ export default function Navbar() {
           </Link>
         </li>
       </ul>
-      <div className="w-6/12 md:w-3/12 visible md:invisible flex justify-end items-center">
+      <div className="w-6/12 md:w-3/12  flex justify-end items-center">
+        <div className="invisible md:visible relative inline-block text-left">
+          <button
+            onClick={() => setDropdownOpen(!isDropdownOpen)}
+            className="inline-flex items-center w-15 px-2 py-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none"
+          >
+            {selectedLanguage} <span className="ml-3 text-xs">▼</span>
+          </button>
+
+          {isDropdownOpen && (
+            <div className="absolute left-0 mt-2 w-20 bg-white border border-gray-300 rounded-md shadow-md">
+              <button
+                onClick={() => {
+                  setSelectedLanguage("English");
+                  setDropdownOpen(!isDropdownOpen);
+                }}
+                className="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+              >
+                English
+              </button>
+              <button
+                onClick={() => {
+                  setSelectedLanguage("Español");
+                  setDropdownOpen(!isDropdownOpen);
+                }}
+                className="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+              >
+                Español
+              </button>
+            </div>
+          )}
+        </div>
+
         <button
-          className="font-bold sans_serif text-2xl text-right mr-12"
+          className="visible md:invisible font-bold sans_serif text-2xl text-right mr-12"
           onClick={() => setmenuActive(true)}
         >
           ☰
@@ -62,10 +97,16 @@ export default function Navbar() {
             >
               Close
             </p>
-            <ul className="mx-5 mt-5"> 
-              <li className="cursor-pointer py-1 border-t group"><span className="group-hover:border-b">About Me</span></li>
-              <li className="cursor-pointer py-1 border-t group"><span className="group-hover:border-b">My Skills</span></li>
-              <li className="cursor-pointer py-1 border-t group border-b"><span className="group-hover:border-b"> My Projects</span></li>
+            <ul className="mx-5 mt-5">
+              <li className="cursor-pointer py-1 border-t group">
+                <span className="group-hover:border-b">About Me</span>
+              </li>
+              <li className="cursor-pointer py-1 border-t group">
+                <span className="group-hover:border-b">My Skills</span>
+              </li>
+              <li className="cursor-pointer py-1 border-t group border-b">
+                <span className="group-hover:border-b"> My Projects</span>
+              </li>
             </ul>
           </div>
         )}
